cd6482aec62ca1337685257eae36716afe9053bb
luisworker <luisworker2010@gmail.com>
2020-09-27 22:20:48 +0200

    Modelo de Json

---
+++ ASP-api/Models/ws/Reply.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_api.Models.ws
+{
+    public class Reply
+    {
+        public bool Ok { get; set; }
+        public object Datos { get; set; }
+        public string Message { get; set; }
+    }
+}
