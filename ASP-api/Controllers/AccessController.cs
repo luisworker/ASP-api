1e8e80d4c01c3012c31c62e5e4e350db005b02bf
luisworker <luisworker2010@gmail.com>
2020-09-29 12:22:27 +0200

    Posman parametros con el body

--- ASP-api/Controllers/AccessController.cs
+++ ASP-api/Controllers/AccessController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ASP_api.Models;
 using ASP_api.Models.ws;
 namespace ASP_api.Controllers
 {
@@ -17,6 +18,38 @@ namespace ASP_api.Controllers
             reply.Message = "hola mundo";
             //string  serjson=JsonConvert.SerializeObject(reply);
             //return serjson;
+            return reply;
+        }
+        [HttpPost]
+        public Reply Login([FromBody]VMLoguin login) {
+            var reply = new Reply();
+            reply.Ok = false;
+            try
+            {
+                using (APIProyectEntities db=new APIProyectEntities())
+                {
+                    var lst = db.User.Where(d=>d.email==login.Email && d.pass==login.Password&&d.status==1);
+                    if (lst.Count()>0)
+                    {
+                        reply.Ok = true;
+                        var guid = Guid.NewGuid().ToString();
+                        reply.Datos = guid;
+                        lst.First().token = guid;
+                        db.SaveChanges();
+                        reply.Message = "OKOK";
+                    }
+                    else
+                    {
+                        reply.Message = "Datos Incorrectos";
+                    }
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                reply.Ok = false;
+                reply.Message = "Error En autentificacion: "+ex.Message;
+            }
             return reply;
         }
     }
---
+++ ASP-api/Models/VMloguin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_api.Models
+{
+    public class VMLoguin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
